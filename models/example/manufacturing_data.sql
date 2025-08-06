7eec0ccf11dd279cf1b61bc9994ddbad24447aad
tanimabehera123 <Tanimarani.Behera@iseyon.com>
2025-08-06 12:36:51 +0530

    small fixes

--- models/example/manufacturing_data.sql
+++ models/example/manufacturing_data.sql
@@ -1,1 +1,9 @@
-select * from manufacturing
+SELECT
+  product_id,
+  product_name,
+  category,
+  production_date,
+  quantity,
+  cost_per_unit,
+  quantity * cost_per_unit AS total_cost
+FROM  manufacturing
